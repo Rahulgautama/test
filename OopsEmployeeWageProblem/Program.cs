dbbe265c197b342ec864a5bfcd5b8b398b1ec000
Rahulgautama <rahul8874950727@gmail.com>
2023-05-07 19:05:35 +0530

    Welcome To Employee Wage Computation using Oops Concept

---
+++ OopsEmployeeWageProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OopsEmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program using Oops Concept");
+        }
+    }
+}
